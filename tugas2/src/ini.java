ca64b81023ebcf19a9fc84ee34a538974dd4ecfc
FaturRumahan <81752796+faturrumahan@users.noreply.github.com>
2021-04-11 18:44:06 +0700

    Update ini.java

--- tugas2/src/ini.java
+++ tugas2/src/ini.java
@@ -13,7 +13,7 @@ public class ini {
 
         try {
             System.out.print("Pilih : ");
-            pilih = input.nextInt(); // Read user input
+            pilih = input.nextInt(); // ketika pemilihan menu, user input nama "ex : mobil" bukan angka disamping
         } catch (InputMismatchException error) {
             System.out.println("errornya adalah " + error.getMessage());
         } finally {
@@ -24,9 +24,9 @@ public class ini {
                     System.out.print("Masukkan merk : ");
                     a = input.nextLine();
                     System.out.print("Masukkan horse power : ");
-                    c = input.nextInt();
+                    c = input.nextInt(); //ketika memasukkan hp, mungkin user menginputkan hp bukan dengan angka atau ditambah satuan "ex : 50hp"
                     System.out.print("Masukkan harga : ");
-                    d = input.nextInt();
+                    d = input.nextInt(); //ketika memasukkan harga, mungkin user menginputkan harga bukan dengan angka
                     input.nextLine();
                     System.out.print("Masukkan warna : ");
                     b = input.nextLine();
@@ -47,9 +47,9 @@ public class ini {
                     System.out.print("Masukkan merk : ");
                     a = input.nextLine();
                     System.out.print("Masukkan horse power : ");
-                    c = input.nextInt();
+                    c = input.nextInt(); //ketika memasukkan hp, mungkin user menginputkan hp bukan dengan angka atau ditambah satuan "ex : 50hp"
                     System.out.print("Masukkan harga : ");
-                    d = input.nextInt();
+                    d = input.nextInt(); //ketika memasukkan harga, mungkin user menginputkan harga bukan dengan angka
                 } catch (Exception error) {
                     System.out.println("errornya adalah " + error.getMessage());
                 } finally {
