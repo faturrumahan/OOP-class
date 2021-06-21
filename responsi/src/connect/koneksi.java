54e39ddb9e9044bb691eac6e97d2425c7beac5b4
FaturRumahan <81752796+faturrumahan@users.noreply.github.com>
2021-06-21 18:41:38 +0700

    Update koneksi.java

--- responsi/src/connect/koneksi.java
+++ responsi/src/connect/koneksi.java
@@ -10,8 +10,8 @@ public class koneksi {
         {
             try {
                 String url="jdbc:mysql://localhost/responsipbo";
-                String username= "root";     // default username
-                String password= "";         // jika ada password bisa anda masukkan
+                String username= "root";     
+                String password= "";         
                 
                 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
                 koneksi = DriverManager.getConnection(url, username, password);
